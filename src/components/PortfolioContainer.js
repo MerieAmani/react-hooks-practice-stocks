9129bef70694f6fa093cf377b900b99c26735d51
Matthew <meriematthew01@gmail.com>
2025-04-29 01:14:00 +0300

    react practice stocks

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Stock from "./Stock";
 
 function PortfolioContainer() {
+  const [portfolio, setPortfolio] = useState([]);
+
+  function handleSellStock(stockToSell) {
+    setPortfolio(portfolio.filter((stock) => stock.id !== stockToSell.id));
+  }
+
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      {portfolio.map((stock) => (
+        <Stock key={stock.id} stock={stock} onClick={() => handleSellStock(stock)} />
+      ))}
     </div>
   );
 }
